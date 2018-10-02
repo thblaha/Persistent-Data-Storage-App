9a745a0b06214e10c8b689f848758161413c6c52
Veronica Blaha <veronica.blaha@gmail.com>
2018-10-02 18:03:20 -0400

    fixed database issue

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-// -----------------
-mongoose.connect("mongodb://user:password1@ds121203.mlab.com:21203/heroku_2z7j0jbv", { useNewUrlParser: true });
+// -----------------mongodb://<dbuser>:<dbpassword>@ds121203.mlab.com:21203/heroku_2z7j0jbv
+mongoose.connect("mongodb://test2:lab_2018A@ds121203.mlab.com:21203/heroku_2z7j0jbv", { useNewUrlParser: true });
 // mongoose.connect('mongodb://localhost:27017/tododb')
 require('./routes/api-routes.js')(app);
 require('./routes/html-routes.js')(app);
